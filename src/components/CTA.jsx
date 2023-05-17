4b11ad1af8d0b62013e1aefce50fbed3c44419dc
Paul Okwulu <pauldevcodes@gmail.com>
2023-05-17 14:08:44 +0100

    input validation done

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,6 +1,24 @@
 import { Form } from "react-router-dom";
+import { useState } from 'react';
 
 const CTA = () => {
+
+    const [email, setEmail] = useState("")
+    const [valid, setValid] = useState(true)
+
+    const validateEmail = (e) => {
+        const regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+        // console.log(regex.test(email))
+        if (regex.test(email)) {
+            setEmail("")
+            setValid(true)
+            return true
+        } else {
+            setValid(!valid)
+            return false
+        }
+    }
+
     return (
         <div className=" bg-primaryTwo px-12 pt-[54px] pb-[71px] flex flex-col items-center gap-y-6 xl:px-20 xl:pt-[100px] xl:pb-[133px] xl:flex-row xl:gap-x-[227px]">
             <div className=" flex flex-col items-center gap-y-4 xl:basis-1/2 xl:items-start xl:gap-y-[17px]">
@@ -12,12 +30,20 @@ const CTA = () => {
                 </p>
             </div>
             <Form className=" xl:basis-1/2">
-                <input
-                    type="email"
-                    placeholder='email@example.com'
-                    className=' font-["Raleway"] text-xs leading-[14px] tracking-[0.4px] text-neutralOne pt-[14px] pb-3 pl-[21px] w-full rounded-[3px]'
-                />
-                <button className=' font-["Raleway"] font-bold text-sm leading-4 text-white bg-accentOne py-3 rounded-[3px] w-full mt-2 xl:w-fit xl:py-4 xl:px-[22px] hover:opacity-90'>
+                <div className=" w-full">
+                    <input
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="email"
+                        placeholder='email@example.com'
+                        className=' font-["Raleway"] text-xs leading-[14px] tracking-[0.4px] text-neutralOne pt-[14px] pb-3 pl-[21px] w-full rounded-[3px]'
+                    />
+                    {valid ? "" : <p className=' text-sm text-center text-white'>Please check your email</p>}
+                </div>
+                <button
+                    onClick={validateEmail}
+                    className=' font-["Raleway"] font-bold text-sm leading-4 text-white bg-accentOne py-3 rounded-[3px] w-full mt-2 xl:w-fit xl:py-4 xl:px-[22px] hover:opacity-9z0'
+                >
                     Get Started For Free
                 </button>
             </Form>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,26 @@
 import { Form } from 'react-router-dom';
 import illustrationOne from '../images/illustration-1.svg'
+import { useState } from 'react';
 
 const Hero = () => {
+
+    const [email, setEmail] = useState("")
+    const [valid, setValid] = useState(true)
+
+    const validateEmail = (e) => {
+        const regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+        // console.log(regex.test(email))
+        if (regex.test(email)) {
+            setEmail("")
+            setValid(true)
+            return true
+        } else {
+            setValid(!valid)
+            return false
+        }
+    }
+
+
     return (
         <div className=" mx-7 mb-[90px] flex flex-col gap-y-10 xl:mx-20 xl:mb-[164px] xl:flex-row-reverse xl:items-center xl:gap-x-[55px]">
             <img src={illustrationOne} alt="an illustration of people arranging files" className=' xl:w-[640px] xl:basis-1/2' />
@@ -12,15 +31,24 @@ const Hero = () => {
                 <p className=' font-["Open Sans"] text-sm leading-[21px] text-center text-primaryOne mt-6 mb-8 xl:text-[17px] xl:leading-6 xl:text-start xl:tracking-[1px]'>
                     Fylo stores your most important files in one secure location. Access them wherever you need, share and collaborate with friends, family, and co-workers.
                 </p>
+
                 <Form
-                className=' flex flex-col gap-y-4 xl:flex-row xl:gap-x-4'
+                    className=' flex flex-col gap-y-4 xl:flex-row xl:gap-x-4'
                 >
-                    <input
-                        type="email"
-                        placeholder='Enter your email..'
-                        className=' font-["Raleway"] text-xs leading-[14px] tracking-[0.4px] text-neutralTwo pt-[14px] pb-3 pl-[21px] w-full rounded-[3px] xl:leading-[19px]'
-                    />
-                    <button className=' font-["Raleway"] font-bold text-sm leading-4 text-white bg-accentOne py-3 rounded-[3px] w-full hover:opacity-90'>
+                    <div className=' w-full'>
+                        <input
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            type="email"
+                            placeholder='Enter your email..'
+                            className=' font-["Raleway"] text-xs leading-[14px] tracking-[0.4px] text-neutralTwo pt-[14px] pb-3 pl-[21px] w-full rounded-[3px] xl:leading-[19px]'
+                        />
+                        {valid ? "" : <p className=' text-red-400'>Please check your email</p>}
+                    </div>
+                    <button
+                        onClick={validateEmail}
+                        className=' font-["Raleway"] font-bold text-sm leading-4 text-white bg-accentOne py-3 rounded-[3px] w-full hover:opacity-90'
+                    >
                         Get Started
                     </button>
                 </Form>
